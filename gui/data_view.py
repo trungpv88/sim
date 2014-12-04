1519bf8026c6fb43c41ea8614073ac270ca34273
trungpv88 <trungpv88@gmail.com>
2014-12-04 14:54:59 +0700

    Add icons + play pronunciation function

--- gui/data_view.py
+++ gui/data_view.py
@@ -1,5 +1,6 @@
 __author__ = 'trungpv'
 from ObjectListView import ObjectListView, ColumnDefn
+from word.word import Word
 import wx
 
 
@@ -17,8 +18,13 @@ class MainPanel(wx.Panel):
     """
     http://www.blog.pythonlibrary.org/2009/12/23/wxpython-using-objectlistview-instead-of-a-listctrl/
     """
-    def __init__(self, parent, word_list):
+    def __init__(self, parent, word_list, dict_db):
         wx.Panel.__init__(self, parent=parent, id=wx.ID_ANY)
+        self.word_list = word_list
+        self.w_l = {}
+        self.normalize_word_list(word_list)
+        self.dict_db = dict_db
+        self.num_word = len(self.word_list)
         self.words = []
         # data overlay in list view
         self.dataOlv = ObjectListView(self, wx.ID_ANY, style=wx.LC_REPORT | wx.SUNKEN_BORDER)
@@ -29,27 +35,55 @@ class MainPanel(wx.Panel):
         main_sizer.Add(self.dataOlv, 1, wx.ALL | wx.EXPAND, 5)
         self.SetSizer(main_sizer)
         # load data to overlay
-        self.load_data(word_list)
+        self.load_data()
 
-    def dict_to_word_obj(self, word_list):
+    def normalize_word_list(self, word_list):
+        """
+        Normalize content of word list.
+        For example: {'hello': ['1. Hi', '2. Say in meeting']}
+        :param word_list:
+        :return:
+        """
+        for k, v in word_list.items():
+            lines = v.split('\n')
+            normal_lines = []
+            line_num = 1
+            for line in lines:
+                if line is not '':
+                    normal_line = line.replace(':', '.')  # replace last character from ':' to '.'
+                    # upper first character and insert space at the beginning of line
+                    if normal_line[0] is not ' ':
+                        normal_line = ' ' + normal_line[0].upper() + normal_line[1:]
+                    else:
+                        normal_line = normal_line[0] + normal_line[1].upper() + normal_line[2:]
+                    # insert definition number at beginning of line
+                    normal_line = str(line_num) + '.' + normal_line
+                    normal_lines.append(normal_line)
+                    line_num += 1
+            self.w_l[k] = normal_lines
+
+    def dict_to_word_obj(self):
         """
         Convert from dictionary to WordView object
         :param word_list:dictionary
         :return:
         """
         word_id = 1
-        for k, v in word_list.items():
-            tmp_obj = WordView(word_id, k, v.split('\n')[0])
+        for k, v in self.w_l.items():
+            # tmp_obj = WordView(word_id, k, v[0][3:100] + '..')
+            def_view = ' '.join(v[0].split()[1:15])  # get 15 first words in definition
+            def_view = def_view.translate(None, '.,') + ' ...'  # remove punctuations
+            tmp_obj = WordView(word_id, k, def_view)
             self.words.append(tmp_obj)
             word_id += 1
 
-    def load_data(self, word_list):
+    def load_data(self, ):
         """
         Load data from database to display on overlay
         :param word_list:
         :return:
         """
-        self.dict_to_word_obj(word_list)
+        self.dict_to_word_obj()
         self.dataOlv.SetObjects(self.words)
 
     def add_new_data(self, new_word):
@@ -58,8 +92,20 @@ class MainPanel(wx.Panel):
         :param new_word:
         :return:
         """
-        self.words.append(new_word)
-        self.dataOlv.SetObjects(self.words)
+        # check whether new word exists or is blank
+        if new_word not in self.word_list.keys() and new_word != "":
+            w = Word(value=new_word)
+            word_def = w.get_definition()
+            if word_def is not '':
+                self.word_list[new_word] = word_def
+                # save new word to database
+                self.dict_db.save(self.word_list)
+                self.num_word += 1
+                self.words.append(WordView(self.num_word, new_word, word_def))
+                # display new word on overlay
+                self.dataOlv.SetObjects(self.words)
+            # get new word pronunciation
+            w.get_pronunciation()
 
     def set_columns(self):
         """
@@ -69,6 +115,32 @@ class MainPanel(wx.Panel):
         self.dataOlv.SetColumns([
             ColumnDefn('No', 'center', 50, 'id'),
             ColumnDefn('Word', 'left', 100, 'value'),
-            ColumnDefn('Definition', 'left', 700, 'definition')
+            ColumnDefn('Definition', 'left', 600, 'definition')
         ])
         self.dataOlv.SetObjects(self.words)
+
+    def play_pronunciation(self, e):
+        """
+        Event raises when play button is clicked
+        :return:
+        """
+        selected_obj = self.dataOlv.GetSelectedObjects()
+        for obj in selected_obj:
+            w = Word(obj.value)
+            w.pronounce()
+
+    def view_definition(self, e):
+        """
+        Event raises when view definition button is clicked
+        :param e:
+        :return:
+        """
+        selected_obj = self.dataOlv.GetSelectedObjects()
+        word_def = ""
+        for obj in selected_obj:
+            word_def += obj.value.upper() + '\n'
+            for line in self.w_l[obj.value]:
+                word_def += line.decode('utf-8') + '\n'
+            word_def += '-------------------------------------------\n'
+        msg_box = wx.MessageDialog(None, word_def, 'Word Definition', style=wx.OK | wx.ICON_INFORMATION)
+        msg_box.ShowModal()
--- gui/window.py
+++ gui/window.py
@@ -1,5 +1,6 @@
 __author__ = 'trungpv'
 import wx
+import  wx.calendar
 from word.word import Word
 from data_view import MainPanel, WordView
 
@@ -12,10 +13,7 @@ class WindowManager(wx.Frame):
     def __init__(self, parent, title, word_list, dict_db):
         super(WindowManager, self).__init__(parent, title=title, size=(800, 600))
         self.CenterOnScreen()
-        self.word_list = word_list
-        self.dict_db = dict_db
-        self.panel = MainPanel(self, self.word_list)
-        self.num_word = len(self.word_list)
+        self.panel = MainPanel(self, word_list, dict_db)
         self.basic_gui()
 
     def basic_gui(self):
@@ -28,22 +26,47 @@ class WindowManager(wx.Frame):
         menu_bar = wx.MenuBar()
 
         file_btn = wx.Menu()
-        about_btn = wx.Menu()
+        help_btn = wx.Menu()
 
         exit_item = wx.MenuItem(file_btn, wx.ID_EXIT, 'Exit', '...')
         file_btn.AppendItem(exit_item)
         self.Bind(wx.EVT_MENU, self.quit, exit_item)
+        about_item = wx.MenuItem(help_btn, wx.ID_ABOUT, 'About', '...')
+        help_btn.AppendItem(about_item)
+        self.Bind(wx.EVT_MENU, self.about, about_item)
+        guide_item = wx.MenuItem(help_btn, wx.ID_HELP, 'Help Topic', '...')
+        help_btn.AppendItem(guide_item)
+        self.Bind(wx.EVT_MENU, self.guide, guide_item)
 
         menu_bar.Append(file_btn, '&File')
-        menu_bar.Append(about_btn, '&About')
+        menu_bar.Append(help_btn, '&Help')
 
         self.SetMenuBar(menu_bar)
 
     def tool_bar_design(self):
         tool_bar = self.CreateToolBar()
-        add_tool_btn = tool_bar.AddLabelTool(wx.ID_ANY, 'Add new word', wx.Bitmap('icon/add_word.ico'))
+        add_tool_btn = tool_bar.AddLabelTool(wx.ID_ANY, 'Add', wx.Bitmap('icon/add_word.ico'), shortHelp='Add new word')
+        play_tool_btn = tool_bar.AddLabelTool(wx.ID_ANY, 'Play',wx.Bitmap('icon/play_pronunciation.ico'),
+                                              shortHelp='Play word pronunciation')
+        define_tool_btn = tool_bar.AddLabelTool(wx.ID_ANY, 'Definition', wx.Bitmap('icon/view_definition.ico'),
+                                                shortHelp='View word definitions')
+        language_tool_btn = tool_bar.AddLabelTool(wx.ID_ANY, 'Languages', wx.Bitmap('icon/dictionary.ico'),
+                                                  shortHelp='Change learning language')
+        calendar_tool_btn = tool_bar.AddLabelTool(wx.ID_ANY, 'Calendar', wx.Bitmap('icon/calendar.ico'),
+                                                  shortHelp='Track learning')
+        test_tool_btn = tool_bar.AddLabelTool(wx.ID_ANY, 'Test', wx.Bitmap('icon/test.ico'),
+                                              shortHelp='Test your vocabulary')
+        setting_tool_btn = tool_bar.AddLabelTool(wx.ID_ANY, 'Setting', wx.Bitmap('icon/setting.ico'),
+                                                 shortHelp='Configure servers')
+
         tool_bar.Realize()
         self.Bind(wx.EVT_TOOL, self.add_word, add_tool_btn)
+        self.Bind(wx.EVT_TOOL, self.panel.play_pronunciation, play_tool_btn)
+        self.Bind(wx.EVT_TOOL, self.panel.view_definition, define_tool_btn)
+        self.Bind(wx.EVT_TOOL, self.change_language, language_tool_btn)
+        self.Bind(wx.EVT_TOOL, self.track_learning, calendar_tool_btn)
+        self.Bind(wx.EVT_TOOL, self.test_vocabulary, test_tool_btn)
+        self.Bind(wx.EVT_TOOL, self.configure_server, setting_tool_btn)
 
     def tool_strip_design(self):
         print ''
@@ -58,6 +81,36 @@ class WindowManager(wx.Frame):
         if yes_no_box.ShowModal() == wx.ID_YES:
             self.Close()
 
+    def about(self, e):
+        """
+        Event raises when about menu bar button is clicked
+        :param e:
+        :return:
+        """
+        info = wx.AboutDialogInfo()
+        info.SetIcon(wx.Icon('icon/logo.png', wx.BITMAP_TYPE_PNG))
+        info.SetName('Sim')
+        info.SetVersion('1.0')
+        sim_description = """
+        Sim is an cross-platform application (support for Windows and Linux) helping user learn
+        vocabulary. The new words are imported automatically from google search (for pronunciation)
+        and website: http://www.oxforddictionaries.com (for definition).
+        It's developed in python 2.7 with some modules such as pygame, pydub (for sound), wxPython (for GUI),
+        ffmpeg (dependency) ... The set of icons is found in: http://www.fatcow.com/ """
+        sim_license = """
+        Sim is free software; you can redistributeit and/or modify it under the terms of the GNU General Public
+        License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any
+        later version. """
+        info.SetDescription(sim_description)
+        info.SetCopyright('(C) 2014 Pham Van Trung')
+        info.SetWebSite('https://github.com/trungpv88')
+        info.AddDeveloper('trungpv88@gmail.com')
+        info.SetLicence(sim_license)
+        wx.AboutBox(info)
+
+    def guide(self, e):
+        print 'help'
+
     def add_word(self, e):
         """
         Event raises when add word button is clicked
@@ -67,16 +120,21 @@ class WindowManager(wx.Frame):
         name_box = wx.TextEntryDialog(None, 'Please enter a new word: ', 'Sim', '')
         if name_box.ShowModal() == wx.ID_OK:
             new_word = name_box.GetValue()
-            # check whether new word exists or is blank
-            if new_word not in self.word_list.keys() and new_word != "":
-                w = Word(value=new_word)
-                word_def = w.get_definition()
-                if word_def is not "":
-                    self.word_list[new_word] = word_def
-                    # save new word to database
-                    self.dict_db.save(self.word_list)
-                    self.num_word += 1
-                    # display new word on overlay
-                    self.panel.add_new_data(WordView(self.num_word, new_word, word_def))
-                # get new word pronunciation
-                w.get_pronunciation()
+            self.panel.add_new_data(new_word)
+
+    def change_language(self, e):
+        choose_language = wx.SingleChoiceDialog(None, 'Choose your learning language:', 'Language',
+                                                ['English', 'French'])
+        if choose_language.ShowModal() == wx.ID_OK:
+            print choose_language.GetStringSelection()
+
+    def track_learning(self, e):
+        wx.calendar.CalendarCtrl(self, -1, wx.DateTime_Now(), pos=(25, 50),
+                                 style=wx.calendar.CAL_SHOW_HOLIDAYS | wx.calendar.CAL_SUNDAY_FIRST |
+                                       wx.calendar.CAL_SEQUENTIAL_MONTH_SELECTION)
+
+    def test_vocabulary(self, e):
+        print 'x'
+
+    def configure_server(self, e):
+        print 'y'
