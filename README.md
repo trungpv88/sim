d47071c5c0e80512cbc935c04aabf0fded5624f8
trungpv88 <trungpv88@gmail.com>
2015-01-16 16:08:42 +0700

    Update version 1.1

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 SIM application
 ===
 
-An cross-platform application (support for Windows and Linux) help user learn vocabulary and manage
-their english-french learning period. The new words are imported automatically from google search 
-(to get word pronunciation and images) and website: http://www.oxforddictionaries.com (to get word definition). 
+An cross-platform application (support for Windows and Linux) help users learn vocabulary and manage
+their foreign languages (english and french) learning period. Some information of a new entered words are 
+imported automatically from:
++ google search (to get word pronunciation and description images) 
++ website: http://www.oxforddictionaries.com (to get word definition, pronunciation and line breaks)
+Users can track their learning period with a word number line chart. For testing the memory, this application 
+provides a test functionality by listening some words then writing the answers. Moreover, it allow to take 
+note about learnt phrases/sentences in both languages with (if any)their pronunciation
 
-It's developed in python 2.7 with some modules such as pygame, pydub (for sound), wxPython (version 2.8x 
-recommended) (for GUI), ffmpeg (dependency) ... The free set of icons is found in: http://www.fatcow.com/
+SIM is developed in python 2.7 with some modules such as pygame, pydub (for sound), wxPython (version 2.8x 
+recommended) (for GUI), ffmpeg (dependency) ... 
+
+For the icons set, please find out more in: http://www.fatcow.com/
+Besides, some Yahoo Messenger 6.0 sounds are used for the closing and opening form or clicking button events.
--- gui/menu_bar.py
+++ gui/menu_bar.py
@@ -100,19 +100,28 @@ class MenuBar(object):
         info = wx.AboutDialogInfo()
         info.SetIcon(wx.Icon('icon/logo.png', wx.BITMAP_TYPE_PNG))
         info.SetName('Sim')
-        info.SetVersion('1.0')
+        info.SetVersion('1.1')
         sim_description = """
-        Sim is an cross-platform application (support for Windows and Linux) helping users learn vocabulary and manage
-        their english-french learning period. The new words are imported automatically from google search (to get
-        pronunciation and images) and website: http://www.oxforddictionaries.com (to get word definition). It's
-        developed in python 2.7 using some modules such as pygame, pydub (for sound), wxPython (version 2.8x
-        recommended) (for GUI), ffmpeg (dependency) ... The free set of icons is found in: http://www.fatcow.com/ """
+        An cross-platform application (support for Windows and Linux) help users learn vocabulary and manage
+        their foreign languages (english and french) learning period. Some information of a new entered words are
+        imported automatically from:
+        + google search (to get word pronunciation and description images)
+        + website: http://www.oxforddictionaries.com (to get word definition, pronunciation and line breaks)
+        Users can track their learning period with a word number line chart. For testing the memory, this application
+        provides a test functionality by listening some words then writing the answers. Moreover, it allow to take
+        note about learnt phrases/sentences in both languages with (if any)their pronunciation
+
+        SIM is developed in python 2.7 with some modules such as pygame, pydub (for sound), wxPython (version 2.8x
+        recommended) (for GUI), ffmpeg (dependency) ...
+
+        For the icons set, please find out more in: http://www.fatcow.com/
+        Besides, some Yahoo Messenger 6.0 sounds are used for the closing and opening form or clicking button events."""
         sim_license = """
         Sim is free software; you can redistribute it and/or modify it under the terms of the GNU
         General Public License as published by the Free Software Foundation;
         either version 2 of the License, or (at your option) any later version. """
         info.SetDescription(sim_description)
-        info.SetCopyright('(C) 2014 Pham Van Trung')
+        info.SetCopyright('(C) 2015 Pham Van Trung')
         info.SetWebSite('http://vn.linkedin.com/in/trungpv88')
         info.AddDeveloper('trungpv88@gmail.com')
         info.SetLicence(sim_license)
